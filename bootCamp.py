f91468f00c608d10759a0e5f8b2ee10b22b2557d
Hyojoo <kangh83@zedat.fu-berlin.de>
2024-08-19 22:44:19 +0200

    Day1[input()]

--- bootCamp.py
+++ bootCamp.py
@@ -7,5 +7,6 @@ print("Hello, World!\n  Hallo Kaeng :) ")
 #concatenate: String 연결 
 print("Guten Tag" +" "+ "Kaeng")
 
-
+#input () function : a prompt for user 
+print("Hello" + " " + input("What is your name?") + "!") 
 

3ac46f9980957d49f01fde4205e8eea8ddada37c
Hyojoo <kangh83@zedat.fu-berlin.de>
2024-08-19 22:52:12 +0200

    Day1[len()]

--- bootCamp.py
+++ bootCamp.py
@@ -2,11 +2,16 @@
 #Day1
 
 #new line: \n
-print("Hello, World!\n  Hallo Kaeng :) ")
+#print("Hello, World!\n  Hallo Kaeng :) ")
 
 #concatenate: String 연결 
-print("Guten Tag" +" "+ "Kaeng")
+#print("Guten Tag" +" "+ "Kaeng")
 
 #input () function : a prompt for user 
-print("Hello" + " " + input("What is your name?") + "!") 
+#print("Hello" + " " + input("What is your name?") + "!") 
 
+#excercise1 : 입력된 문자 길이 구하기 
+name = input("What's your name? \n")
+print(len(name))
+
+ 
